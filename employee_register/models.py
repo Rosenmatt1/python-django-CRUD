f970d49caa7203334cbe1454db9ff5d2a2b08684
Mateo <rosenmatt1@gmail.com>
2020-06-20 17:05:40 -0600

    Added Employee and Position classes in employee_register app

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    title = models.Charfield(max_length=50)
+
+class Employee(models.Model):
+    fullname = models.Charfield(max_length=100)
+    emp_code = models.Charfield(max_length=3)
+    mobile = models.Charfield(max_length=15)
+    position = models.Charfield(max_length=100)
